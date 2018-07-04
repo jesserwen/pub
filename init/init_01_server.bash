be17e0ccd1098695a990c508431fe2fad82b6613
Xizhen Du <xizhen.du@ge.com>
2018-07-04 13:53:26 +0800

    Wed Jul  4 13:53:26 CST 2018

--- init/init_01_server.bash
+++ init/init_01_server.bash
@@ -3,8 +3,7 @@ yum install -y nginx
 # apt-get install -y nginx
 
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
-wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/xizhendu/pub/master/init/init_01_nginx.conf
-
+wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/xizhendu/pub/master/init/nginx.conf
 
 
 mkdir -p /usr/share/nginx/html/kickstart
