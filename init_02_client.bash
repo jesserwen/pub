c0bbbebe6fca7488a32cd454e2309495de7981ab
Xizhen Du <xizhendu@gmail.com>
2018-07-04 11:21:29 +0800

    Create init_02_client.bash

---
+++ init_02_client.bash
@@ -0,0 +1,30 @@
+
+#/bin/bash
+
+the_initial_server=$1
+
+mkdir -p /etc/yum.repos.d/tmp/
+mv /etc/yum.repos.d/*.repo /etc/yum.repos.d/tmp/
+
+echo "$the_initial_server   mirrors" >> /etc/hosts
+
+[os]
+name=os
+baseurl=http://mirrors/centos/7/os/x86_64/
+gpgcheck=0
+
+[updates]
+name=updates
+baseurl=http://mirrors/centos/7/updates/x86_64/
+gpgcheck=0
+
+[extras]
+name=extras
+baseurl=http://mirrors/centos/7/extras/x86_64/
+gpgcheck=0
+
+[epel]
+name=epel
+baseurl=http://mirrors/epel/7/x86_64/
+gpgcheck=0
+_eof_
